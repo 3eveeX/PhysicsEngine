ff6ac5cb8d67294b11b1793835f6786d8290d02a
3eveeX <hatchjavan@gmail.com>
2026-04-02 11:20:17 -0600

    yep

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,43 @@ by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit h
 */
 
 #include "raylib.h"
+#include "raymath.h"
 
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
+#include <vector>
+
+struct Body{
+	    Vector2 position;
+		Vector2 velocity;
+		Vector2 acceleration;
+		float mass;
+		float size;
+		float restitution;
+};
+
+float GetRandomFloat() {
+	return (float)GetRandomValue(0, 10000) / 10000.0f;
+}
+
+void AddForce(Body& body, Vector2 force)
+{
+	body.acceleration += force * (1/body.mass);
+}
+
+void Integrator(Body& body, float deltaTime)
+{
+	body.velocity += body.acceleration * deltaTime;
+	body.position += body.velocity * deltaTime;
+}
+
+Vector2 gravity{ 0, 9.81f };
 
 int main ()
 {
+
+    std::vector<Body> bodies;
+	bodies.reserve(1000);
+	
 	// Tell the window to use vsync and work on high DPI displays
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
@@ -28,7 +60,59 @@ int main ()
 	// game loop
 	while (!WindowShouldClose())		// run the loop until the user presses ESCAPE or presses the Close button on the window
 	{
-		// drawing
+		float deltaTime = GetFrameTime();
+
+		if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT) || (IsKeyDown(KEY_GRAVE) && IsMouseButtonDown(MOUSE_BUTTON_LEFT)))
+		{
+			Body body;
+			body.position = GetMousePosition();
+			float angle = GetRandomFloat() * (2 * PI);
+			Vector2 direction;
+			direction.x = cosf(angle);
+			direction.y = sinf(angle);
+
+			body.velocity = direction * (50.0f + GetRandomFloat() * 500);
+			body.acceleration = { 0,0 };
+			body.mass = body.size;
+			body.size = GetRandomValue(5, 20);
+			body.restitution =   GetRandomFloat() -0.1f;
+			bodies.push_back(body);
+		}
+
+		//update
+		for (auto& body : bodies)
+		{
+			body.acceleration = { 0, 100 * body.mass };
+			body.velocity.x += body.acceleration.x * deltaTime;
+			body.velocity.y += body.acceleration.y * deltaTime;
+			body.position.x += body.velocity.x * deltaTime;
+			body.position.y += body.velocity.y * deltaTime;
+
+			body.velocity += (gravity * 100) * deltaTime;
+
+			if(body.position.y + body.size> GetScreenHeight())
+			{
+				body.position.y = GetScreenHeight() - body.size;
+				body.velocity.y *= -body.restitution;
+			}
+			if(body.position.x + body.size > GetScreenWidth())
+			{
+				body.position.x = GetScreenWidth() - body.size;
+				body.velocity.x *= -body.restitution;
+			}
+			 else if(body.position.x + body.size < 0)
+			{
+				body.position.x = body.size;
+				body.velocity.x *= -body.restitution;
+			}
+			 else if(body.position.y + body.size < 0)
+			{
+				body.position.y = body.size;
+				body.velocity.y *= -body.restitution;
+			}
+		}
+
+		// draw
 		BeginDrawing();
 
 		// Setup the back buffer for drawing (clear color and depth buffers)
@@ -38,7 +122,13 @@ int main ()
 		DrawText("Hello Raylib", 200,200,20,WHITE);
 
 		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
+		DrawTexture(wabbit, 400, 200, WHITE); 
+
+		for (const auto& body : bodies)
+		 {
+			 
+			 DrawCircleV(body.position, body.size, WHITE);
+		}
 		
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
